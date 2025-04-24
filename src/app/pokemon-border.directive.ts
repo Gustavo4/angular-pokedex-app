8ebddd25bd4561e7ef11fc0067407c490b7ac462
Gustavo4 <gustavordt@outlook.com>
2025-04-24 22:25:51 +0200

    feat: adding new directive for border when mousing over

---
+++ src/app/pokemon-border.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appPokemonBorder]'
+})
+export class PokemonBorderDirective {
+
+  private initialColor: string;
+
+  constructor(private el: ElementRef) { 
+    this.initialColor = this.el.nativeElement.style.borderColor;
+    this.el.nativeElement.style.borderWidhth = '2px';
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    const color = 'green';
+    this.setBorderColor(color);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    const color = this.initialColor;
+    this.setBorderColor(color);
+  }
+
+  private setBorderColor(color: string) {
+    this.el.nativeElement.style.borderColor = color;
+  }
+
+}
