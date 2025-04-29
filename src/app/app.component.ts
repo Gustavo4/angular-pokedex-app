44f07e5912ef2a27876e191fba99f785d6fe5a10
Gustavo4 <gustavordt@outlook.com>
2025-04-29 18:08:34 +0200

    appelle pokemonService au lieu de la constante direct

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { Component, computed, signal } from '@angular/core';
-import { POKEMON_LIST } from './pokemon-list.fake';
+import { Component, inject, signal } from '@angular/core';
 import { Pokemon } from './pokemon.model';
 import { PokemonBorderDirective } from './pokemon-border.directive';
 import { DatePipe } from '@angular/common';
+import { PokemonService } from './pokemon.service';
 
 @Component({
   selector: 'app-root',
   imports: [PokemonBorderDirective, DatePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  pokemonList = signal(POKEMON_LIST);
+  readonly #pokemonService = inject(PokemonService);
+  pokemonList = signal(this.#pokemonService.getPokemonList());
 
   size(pokemon: Pokemon) {
     if (pokemon.life <= 15) {
---
+++ src/app/pokemon.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { POKEMON_LIST } from './pokemon-list.fake';
+import { PokemonList } from './pokemon.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  getPokemonList(): PokemonList {
+    return POKEMON_LIST;
+  } 
+
+  getPokemonById(id: number) {
+    const pokemon = POKEMON_LIST.find(pokemon => pokemon.id === id);
+
+    if (!pokemon) {
+      throw new Error(`No Pokemon found with id ${id}`); 
+    }
+    
+    return pokemon;
+  }
+
+
+  getPokemonTypeList(): string[] {  
+    return [
+      'Feu',
+      'Eau',
+      'Plante',
+      'Electrik',
+      'Vol',
+      'Poison',
+      'Insecte',
+      'Normal',
+      'Fée',
+    ];
+  }
+}
