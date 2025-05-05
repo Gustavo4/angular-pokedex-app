38957d04e2d7be65c5fbe43755062f383c55f572
Gustavo4 <gustavordt@outlook.com>
2025-05-05 23:19:57 +0200

    lier les propriétés de mon formulaire en utilisant formControl directive

--- src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
+++ src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
@@ -1,10 +1,16 @@
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../pokemon.service';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-pokemon-edit',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './pokemon-edit.component.html',
   styles: ``,
 })
@@ -15,4 +21,13 @@ export class PokemonEditComponent {
   readonly pokemon = signal(
     this.pokemonService.getPokemonById(this.pokemonId),
   ).asReadonly();
+
+  readonly form = new FormGroup({
+    name: new FormControl(this.pokemon().name),
+    life: new FormControl(this.pokemon().life),
+    damage: new FormControl(this.pokemon().damage),
+    types: new FormArray(
+      this.pokemon().types.map((type) => new FormControl(type)),
+    ),
+  });
 }
