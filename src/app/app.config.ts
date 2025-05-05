c7eac577c2a101697beba17f5c55cee742307787
Gustavo4 <gustavordt@outlook.com>
2025-05-05 22:44:45 +0200

    adding prettier and creating new edit component and linking in profile component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,36 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
-import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component'; 
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonEditComponent } from './pokemon/pokemon-edit/pokemon-edit.component';
 
 const routes: Routes = [
-  { path: 'pokemons/:id', component: PokemonProfileComponent, title: 'Pokémon' },
-  { path: 'pokemons', component: PokemonListComponent, title: 'Pokédex' },
+  {
+    path: 'pokemons/edit/:id',
+    component: PokemonEditComponent,
+    title: "Édition d'un Pokémon",
+  },
+  {
+    path: 'pokemons/:id',
+    component: PokemonProfileComponent,
+    title: 'Pokémon',
+  },
+  {
+    path: 'pokemons',
+    component: PokemonListComponent,
+    title: 'Pokédex',
+  },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent },
-]
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes)
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
   ],
 };
